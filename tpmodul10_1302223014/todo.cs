aa4864bfae9c66466aad5ea286ae90bae9dfe085
Dafa24 <53173575+Dafa24@users.noreply.github.com>
2024-04-28 23:58:18 +0700

    first commmit api

---
+++ tpmodul10_1302223014/todo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class todo
+{
+    internal readonly bool IsComplete;
+
+    public string Name { get; internal set; }
+
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+    public todo()
+	{
+	}
+}
---
+++ tpmodul10_1302223014/todoDb.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<todo> Todos => Set<todo>();
+}
